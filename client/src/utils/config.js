37ce7520339db712e1ae0a14fbd8aa0b50d0e125
Nabeelcodes110 <parvenabeel@gmail.com>
2023-05-22 06:54:32 +0530

    Final Commit

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -2,23 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBc69xP892s8VN4D_7lB2K6-gjWqUZyq-Y",
-  authDomain: "mercor-task-9c66c.firebaseapp.com",
-  projectId: "mercor-task-9c66c",
-  storageBucket: "mercor-task-9c66c.appspot.com",
-  messagingSenderId: "32485539549",
-  appId: "1:32485539549:web:5cd5afcbdc3c74ca1f6534",
-  measurementId: "G-Z82T23B5KK",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
